fc5ed21a045dbd9b1444d70665fc6e2b3e396f01
whipped cream <eddiebaise@gmail.com>
2016-08-11 12:21:06 -0400

    set profile to default fragment

--- app/src/main/java/com/example/dsuappacademy/fituup/MainActivity.java
+++ app/src/main/java/com/example/dsuappacademy/fituup/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.flContent, new ProfileFragment());
+        tx.commit();
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
